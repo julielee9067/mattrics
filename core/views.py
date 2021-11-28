cd0eb753699d8c5cab46c68748486ae9959508bf
Julie Lee <julielee9067@gmail.com>
2021-11-27 21:29:24 -0500

    Add url for api

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-# from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    dummy = {
+        "stocks": [
+            {"stockName": "Apple", "price": 393.43},
+            {"stockName": "Walmart", "price": 131.47},
+            {"stockName": "Tesla", "price": 1643.00},
+        ]
+    }
+    return HttpResponse(json.dumps(dummy))

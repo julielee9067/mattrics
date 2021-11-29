121232350fbeab86f4c5ec8a7d2af8cf2f6141fa
Julie Lee <julielee9067@gmail.com>
2021-11-28 21:10:15 -0500

    Update dummy data

--- core/views.py
+++ core/views.py
@@ -5,10 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     dummy = {
-        "stocks": [
-            {"stockName": "Apple", "price": 393.43},
-            {"stockName": "Walmart", "price": 131.47},
-            {"stockName": "Tesla", "price": 1643.00},
+        "Adam johnson": [
+            {
+                "seizure occurrences": 2,
+                "breathing pattern": "Low",
+                "GMFCS Level": "IV",
+                "price": 1643.00,
+            },
         ]
     }
     return HttpResponse(json.dumps(dummy))

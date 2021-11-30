68754fc71aad367a75d9e7ac9b12dcb477759ac3
djdanise <daniellese@outlook.com>
2021-11-29 20:11:50 -0500

    Fixed two mux code

--- firmware/two_multiplexer/two_multiplexer.ino
+++ firmware/two_multiplexer/two_multiplexer.ino
@@ -2,10 +2,10 @@
 /////////////////////
 // Pin Definitions //
 /////////////////////
-const int selectPins[3] = {2, 3, 4}; // S0~2, S1~3, S2~4
-const int selectPins_2[3] = {5, 6, 7}; // S0~5, S1~6, S2~7
+const int selectPinsCols[3] = {2, 3, 4}; // S0~2, S1~3, S2~4
+const int selectPinsRows[3] = {8, 9, 10}; // S0~8, S1~9, S2~10
+const int zInput = A0; // Connect col common (Z) to A0 (analog input)
 const int zOutput = 5; 
-const int zInput = A0; // Connect common (Z) to A0 (analog input)
 
 void setup() 
 {
@@ -13,10 +13,10 @@ void setup()
   // Set up the select pins as outputs:
   for (int i=0; i<3; i++)
   {
-    pinMode(selectPins[i], OUTPUT);
-    pinMode(selectPins_2[i], OUTPUT);
-    digitalWrite(selectPins[i], HIGH);
-    digitalWrite(selectPins_2[i], HIGH);
+    pinMode(selectPinsCols[i], OUTPUT);
+    pinMode(selectPinsRows[i], OUTPUT);
+    digitalWrite(selectPinsCols[i], HIGH);
+    digitalWrite(selectPinsRows[i], HIGH);
   }
   pinMode(zInput, INPUT); // Set up Z as an input
   pinMode(zOutput, OUTPUT); // Set up Z as an input
@@ -28,12 +28,14 @@ void setup()
 
 void loop() 
 {
-  // Loop through 6 pins
-  float data[6]= read_mux();
+  // Loop over 6 rows
   for (int i = 0; i < 6; i++){
     write_mux(i);  
+    // Loop over 6 columns
+    float data[6];
+    read_mux(data);
     for (int j = 0; j < 6; j++){
-      Serial.print(String(data[j] + " "));
+      Serial.print(String(data[j]) + " ");
       Serial.println();
     }
   }
@@ -42,33 +44,42 @@ void loop()
 }
 
 void write_mux(int pin){
-  selectMuxPin(pin); // Select one at a time
-  analogWrite(zOutput, 255*5/1023);
+  selectMuxPinWrite(pin); // Select one at a time
+  analogWrite(zOutput, 255);
 }
 
 // Will return 6 entries in 1 row
-float read_mux() {
- float data[6];
+void read_mux(float data[6]) {
  for (byte pin=0; pin<=5; pin++)
   {
-    selectMuxPin(pin); // Select one at a time
+    selectMuxPinRead(pin); // Select one at a time
     int inputValue = analogRead(A0)*0.0048339; // and read Z
 //    Serial.print(String(inputValue) + " ");
     data[pin] = inputValue;
   }
 //  Serial.print("\n");
-  return data;
 }
 
 // The selectMuxPin function sets the S0, S1, and S2 pins
 // accordingly, given a pin from 0-7.
-void selectMuxPin(byte pin)
+void selectMuxPinRead(byte pin)
 {
   for (int i=0; i<3; i++)
   {
     if (pin & (1<<i))
-      digitalWrite(selectPins[i], HIGH);
+      digitalWrite(selectPinsCols[i], HIGH);
     else
-      digitalWrite(selectPins[i], LOW);
+      digitalWrite(selectPinsCols[i], LOW);
+  }
+}
+
+void selectMuxPinWrite(byte pin)
+{
+  for (int i=0; i<3; i++)
+  {
+    if (pin & (1<<i))
+      digitalWrite(selectPinsRows[i], HIGH);
+    else
+      digitalWrite(selectPinsRows[i], LOW);
   }
 }

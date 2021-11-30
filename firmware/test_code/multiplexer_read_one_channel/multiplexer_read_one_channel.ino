37c5706463d41bb12e70e04cae352669476c49d9
sophyli100 <sophyli100@gmail.com>
2021-11-29 21:51:15 -0500

    working 2 multiplexers, organized test/example sketches into folders

--- firmware/test_code/multiplexer_read_one_channel/multiplexer_read_one_channel.ino
+++ firmware/test_code/multiplexer_read_one_channel/multiplexer_read_one_channel.ino
@@ -16,6 +16,18 @@ void write_mux(int pin){
   analogWrite(zOutput, 255);
 }
 
+// Will return 6 entries in 1 row
+void read_mux(float data[6]) {
+ for (byte pin=0; pin<=5; pin++)
+  {
+    selectMuxPinRead(pin); // Select one at a time
+    float inputValue = analogRead(A0)*0.0048339*100; // and read Z
+//    Serial.print(String(inputValue) + " ");
+    data[pin] = inputValue;
+  }
+//  Serial.print("\n");
+}
+
 // The selectMuxPin function sets the S0, S1, and S2 pins
 // accordingly, given a pin from 0-7.
 void selectMuxPinRead(byte pin)
@@ -54,24 +66,32 @@ void setup()
   pinMode(zInput, INPUT); // Set up Z as an input
   pinMode(zOutput, OUTPUT); // Set up Z as an input
 
+  // Print the header:
+//  Serial.println("Y0\tY1\tY2\tY3\tY4\tY5\tY6\tY7");
   Serial.println("Start of program****");
 }
 
 void loop() 
 {
   // Loop over 6 rows
-  for (int i = 0; i < 6; i++){
-    // Turn on row i
-    write_mux(i);
+//  for (int i = 0; i < 6; i++){
+    // Turn on row 0
+    write_mux(5);
     // Loop over 6 columns
-    for (byte pin=0; pin<=5; pin++){
-      selectMuxPinRead(pin); // Select one at a time
-      float inputValue = analogRead(A0)*0.0048339; // and read Z
-      Serial.print(String(inputValue) + " ");
-    }
-      Serial.println();
-    }
-  delay(1000);
-  Serial.print("---------------------------------");  
-  Serial.println();
+//    float data[6] = {0, 0, 0, 0, 0, 0};
+//    read_mux(data);
+     for (byte pin=0; pin<=5; pin++)
+      {
+        selectMuxPinRead(pin); // Select one at a time
+        float inputValue = analogRead(A0)*0.0048339*100; // and read Z
+        Serial.print(String(inputValue) + " ");
+  //      data[pin] = inputValue;
+      }
+//    for (int j = 0; j < 6; j++){
+//    }
+    Serial.println();
+    delay(1000);
+  
+//  Serial.print("--------------------------------");  
+//  Serial.println();
 }

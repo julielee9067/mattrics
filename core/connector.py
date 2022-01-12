5773f636349c7e93f7181688d62f6b1c7a82f4d0
Julie Lee <julielee9067@gmail.com>
2022-01-12 00:28:45 -0500

    Test inserting therapist

--- core/connector.py
+++ core/connector.py
@@ -7,3 +7,24 @@ class DatabaseConnector:
     def __init__(self):
         self.connection = mysql.connector.connect(**MYSQL_SECRET)
         self.cursor = self.connection.cursor()
+
+    def insert_therapist(
+        self, first_name: str, last_name: str, email: str, password: str
+    ):
+        query = (
+            "INSERT INTO Therapist (_id, firstname, lastname, email, password) "
+            "VALUES (%s, %s, %s, %s, %s)"
+        )
+        id = f"{first_name}_{last_name}_{email}"
+        self.cursor.execute(query, (id, first_name, last_name, email, password))
+        self.connection.commit()
+
+
+if __name__ == "__main__":
+    db_connector = DatabaseConnector()
+    db_connector.insert_therapist(
+        first_name="Julie",
+        last_name="Lee",
+        email="julielee9067@gmail.com",
+        password="testpassword",
+    )
